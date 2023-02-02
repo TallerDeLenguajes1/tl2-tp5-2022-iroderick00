fac19b64cac24ab59d2f16dc8532a67b8a5f48d8
Roderick <iroderick@outlook.es>
2023-02-02 19:47:44 -0300

    agrego tabla usuarios y try catchs en el repo

--- CadeteriaWeb/Repositorios/CadeteRepositorio.cs
+++ CadeteriaWeb/Repositorios/CadeteRepositorio.cs
@@ -11,125 +11,167 @@ namespace CadeteriaWeb.Repositorios
 
         public List<Cadete> GetCadetes() 
         {
-            var cadetes = new List<Cadete>();
-            var query = "SELECT * FROM Cadetes WHERE estado = 1";
-            using (var connection = new SqliteConnection(cadenaConexion))
+            try
             {
-                connection.Open();
-                var command = new SqliteCommand(query, connection);
-
-                using (SqliteDataReader reader = command.ExecuteReader())
+                var cadetes = new List<Cadete>();
+                var query = "SELECT * FROM Cadetes WHERE estado = 1";
+                using (var connection = new SqliteConnection(cadenaConexion))
                 {
-                    while (reader.Read())
+                    connection.Open();
+                    var command = new SqliteCommand(query, connection);
+
+                    using (SqliteDataReader reader = command.ExecuteReader())
                     {
-                        var cadete = new Cadete();
-                        cadete.Id = Convert.ToInt32(reader["id"]);
-                        cadete.Nombre = reader["nombre"].ToString();
-                        cadete.Direccion = reader["direccion"].ToString();
-                        cadete.Telefono = reader["telefono"].ToString();
-                        cadetes.Add(cadete);
+                        while (reader.Read())
+                        {
+                            var cadete = new Cadete();
+                            cadete.Id = Convert.ToInt32(reader["id"]);
+                            cadete.Nombre = reader["nombre"].ToString();
+                            cadete.Direccion = reader["direccion"].ToString();
+                            cadete.Telefono = reader["telefono"].ToString();
+                            cadetes.Add(cadete);
+                        }
                     }
+                    connection.Close();
                 }
-                connection.Close();
+                return cadetes; 
             }
-            return cadetes; 
+            catch (Exception)
+            {
+                return new List<Cadete>();
+            }
         }
         public void AltaCadete(string nombre, string direccion, string telefono)
         {
-            var query = $"INSERT INTO Cadetes (nombre, direccion, telefono) VALUES (@nombre,@direccion,@telefono)";
-            using (var connection = new SqliteConnection(cadenaConexion))
+            try
+            {
+                var query = $"INSERT INTO Cadetes (nombre, direccion, telefono) VALUES (@nombre,@direccion,@telefono)";
+                using (var connection = new SqliteConnection(cadenaConexion))
+                {
+
+                    connection.Open();
+
+                    var command = new SqliteCommand(query, connection);
+
+                    command.Parameters.Add(new SqliteParameter("@nombre", nombre));
+                    command.Parameters.Add(new SqliteParameter("@direccion", direccion));
+                    command.Parameters.Add(new SqliteParameter("@telefono", telefono));
+
+                    command.ExecuteNonQuery();
+
+                    connection.Close();
+                }
+            }
+            catch (Exception)
             {
 
-                connection.Open();
-
-                var command = new SqliteCommand(query, connection);
-
-                command.Parameters.Add(new SqliteParameter("@nombre", nombre));
-                command.Parameters.Add(new SqliteParameter("@direccion", direccion));
-                command.Parameters.Add(new SqliteParameter("@telefono", telefono));
-
-                command.ExecuteNonQuery();
-
-                connection.Close();
             }
         }
         public void AltaCadete(Cadete cadete)
         {
-            var query = $"INSERT INTO Cadetes (nombre, direccion, telefono) VALUES (@nombre,@direccion,@telefono)";
-            using (var connection = new SqliteConnection(cadenaConexion))
+            try
+            {
+                var query = $"INSERT INTO Cadetes (nombre, direccion, telefono) VALUES (@nombre,@direccion,@telefono)";
+                using (var connection = new SqliteConnection(cadenaConexion))
+                {
+
+                    connection.Open();
+
+                    var command = new SqliteCommand(query, connection);
+
+                    command.Parameters.Add(new SqliteParameter("@nombre", cadete.Nombre));
+                    command.Parameters.Add(new SqliteParameter("@direccion", cadete.Direccion));
+                    command.Parameters.Add(new SqliteParameter("@telefono", cadete.Telefono));
+
+                    command.ExecuteNonQuery();
+
+                    connection.Close();
+                }
+            }
+            catch (Exception)
             {
 
-                connection.Open();
-
-                var command = new SqliteCommand(query, connection);
-
-                command.Parameters.Add(new SqliteParameter("@nombre", cadete.Nombre));
-                command.Parameters.Add(new SqliteParameter("@direccion", cadete.Direccion));
-                command.Parameters.Add(new SqliteParameter("@telefono", cadete.Telefono));
-
-                command.ExecuteNonQuery();
-
-                connection.Close();
             }
         }
         public Cadete GetCadetePorId (int idCadete)
         {
-            var cadete = new Cadete();
-            var query = "SELECT * FROM Cadetes WHERE id = @idCadete";
-            using (var connection = new SqliteConnection(cadenaConexion))
+            try
             {
-                connection.Open();
-                var command = new SqliteCommand(query, connection);
-                command.Parameters.Add(new SqliteParameter("@idCadete", idCadete));
-
-                using (SqliteDataReader reader = command.ExecuteReader())
+                var cadete = new Cadete();
+                var query = "SELECT * FROM Cadetes WHERE id = @idCadete";
+                using (var connection = new SqliteConnection(cadenaConexion))
                 {
-                    while (reader.Read())
+                    connection.Open();
+                    var command = new SqliteCommand(query, connection);
+                    command.Parameters.Add(new SqliteParameter("@idCadete", idCadete));
+
+                    using (SqliteDataReader reader = command.ExecuteReader())
                     {
-                        cadete.Id = Convert.ToInt32(reader["id"]);
-                        cadete.Nombre = reader["nombre"].ToString();
-                        cadete.Direccion = reader["direccion"].ToString();
-                        cadete.Telefono = reader["telefono"].ToString();
+                        while (reader.Read())
+                        {
+                            cadete.Id = Convert.ToInt32(reader["id"]);
+                            cadete.Nombre = reader["nombre"].ToString();
+                            cadete.Direccion = reader["direccion"].ToString();
+                            cadete.Telefono = reader["telefono"].ToString();
+                        }
                     }
+                    connection.Close();
                 }
-                connection.Close();
+                return cadete;
             }
-            return cadete;
+            catch (Exception)
+            {
+                return new Cadete();
+            }
         }
         public void EditarCadete(Cadete cadeteDatos)
         {
-            var query = $"UPDATE Cadetes SET nombre = @nombre, direccion = @direccion, telefono = @telefono  WHERE id = @idCadete";
-            using (var connection = new SqliteConnection(cadenaConexion))
+            try
+            {
+                var query = $"UPDATE Cadetes SET nombre = @nombre, direccion = @direccion, telefono = @telefono  WHERE id = @idCadete";
+                using (var connection = new SqliteConnection(cadenaConexion))
+                {
+
+                    connection.Open();
+
+                    var command = new SqliteCommand(query, connection);
+
+                    command.Parameters.Add(new SqliteParameter("@idCadete", cadeteDatos.Id));
+                    command.Parameters.Add(new SqliteParameter("@nombre", cadeteDatos.Nombre));
+                    command.Parameters.Add(new SqliteParameter("@direccion", cadeteDatos.Direccion));
+                    command.Parameters.Add(new SqliteParameter("@telefono", cadeteDatos.Telefono));
+
+                    command.ExecuteNonQuery();
+
+                    connection.Close();
+                }
+            }
+            catch (Exception)
             {
 
-                connection.Open();
-
-                var command = new SqliteCommand(query, connection);
-
-                command.Parameters.Add(new SqliteParameter("@idCadete", cadeteDatos.Id));
-                command.Parameters.Add(new SqliteParameter("@nombre", cadeteDatos.Nombre));
-                command.Parameters.Add(new SqliteParameter("@direccion", cadeteDatos.Direccion));
-                command.Parameters.Add(new SqliteParameter("@telefono", cadeteDatos.Telefono));
-
-                command.ExecuteNonQuery();
-
-                connection.Close();
             }
         }
         public void BajaCadete(int idCadete)
         {
-            var query = $"UPDATE Cadetes SET estado = 2  WHERE id = @idCadete";
-            using (var connection = new SqliteConnection(cadenaConexion))
+            try
             {
-                connection.Open();
+                var query = $"UPDATE Cadetes SET estado = 2  WHERE id = @idCadete";
+                using (var connection = new SqliteConnection(cadenaConexion))
+                {
+                    connection.Open();
 
-                var command = new SqliteCommand(query, connection);
+                    var command = new SqliteCommand(query, connection);
 
-                command.Parameters.Add(new SqliteParameter("@idCadete", idCadete));
+                    command.Parameters.Add(new SqliteParameter("@idCadete", idCadete));
 
-                command.ExecuteNonQuery();
+                    command.ExecuteNonQuery();
 
-                connection.Close();
+                    connection.Close();
+                }
+            }
+            catch (Exception)
+            {
+
             }
         }
     }
